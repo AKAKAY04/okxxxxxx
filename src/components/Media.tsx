84149d05811fc936955f92a80cc2ef824fd7193a
AKAKAY04 <151957946+AKAKAY04@users.noreply.github.com>
2023-12-19 02:51:38 +0800

    Add files via upload

---
+++ src/components/Media.tsx
@@ -0,0 +1,53 @@
+import { Box } from "@mui/material";
+import Link from "next/link";
+
+export default function Media() {
+  const mediaList = [
+    {
+      title: "AKAKAY",
+      linkText: "AKAKAY",
+      link: "https://twitter.com/AKAKAY",
+    },
+    {
+      title: "AKAKAY",
+      linkText: "@AKAKAY_",
+      link: "https://twitter.com/AKAKAY",
+    },
+    {
+      title: "Alpha",
+      linkText: "@AKAKAY",
+      link: "https://twitter.com/AKAKAY",
+    },
+  ];
+
+  return (
+    <div className=" py-4">
+      <div className=" flex items-center justify-center gap-x-4 max-sm:flex-col">
+        {mediaList.map(({ title, linkText, link }) => {
+          return (
+            <div
+              key={title}
+              className=" flex items-center gap-2 text-xl"
+            >
+              <span>{title}:</span>
+              <Box
+                component={Link}
+                href={link}
+                className=" hover:underline"
+                sx={{
+                  color: "primary.main",
+                }}
+              >
+                {linkText}
+              </Box>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className=" text-center">
+        打赏地址☕️: AKAKAY.eth
+      </div>
+    </div>
+  );
+}
